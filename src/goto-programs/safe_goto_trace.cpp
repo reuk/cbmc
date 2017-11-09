861b71cba323a13855b9b5e8cb536db5812a4b90
reuk <reuk@users.noreply.github.com>
2017-11-09 22:11:54 +0000

    Add source file

---
+++ src/goto-programs/safe_goto_trace.cpp
@@ -0,0 +1,219 @@
+/*******************************************************************\
+
+Module: Safe traces of GOTO Programs
+
+Author: @reuk
+
+\*******************************************************************/
+
+#include "safe_goto_trace.h"
+
+bool goto_trace_step_baset::is_assignment() const
+{
+  return false;
+}
+
+bool goto_trace_step_baset::is_assume() const
+{
+  return false;
+}
+
+bool goto_trace_step_baset::is_assert() const
+{
+  return false;
+}
+
+bool goto_trace_step_baset::is_goto() const
+{
+  return false;
+}
+
+bool goto_trace_step_baset::is_constraint() const
+{
+  return false;
+}
+
+bool goto_trace_step_baset::is_function_call() const
+{
+  return false;
+}
+
+bool goto_trace_step_baset::is_function_return() const
+{
+  return false;
+}
+
+bool goto_trace_step_baset::is_location() const
+{
+  return false;
+}
+
+bool goto_trace_step_baset::is_output() const
+{
+  return false;
+}
+
+bool goto_trace_step_baset::is_input() const
+{
+  return false;
+}
+
+bool goto_trace_step_baset::is_decl() const
+{
+  return false;
+}
+
+bool goto_trace_step_baset::is_dead() const
+{
+  return false;
+}
+
+bool goto_trace_step_baset::is_shared_read() const
+{
+  return false;
+}
+
+bool goto_trace_step_baset::is_shared_write() const
+{
+  return false;
+}
+
+bool goto_trace_step_baset::is_spawn() const
+{
+  return false;
+}
+
+bool goto_trace_step_baset::is_memory_barrier() const
+{
+  return false;
+}
+
+bool goto_trace_step_baset::is_atomic_begin() const
+{
+  return false;
+}
+
+bool goto_trace_step_baset::is_atomic_end() const
+{
+  return false;
+}
+
+bool goto_trace_step_assignmentt::is_assignment() const
+{
+  return true;
+}
+
+bool goto_trace_step_assumet::is_assume() const
+{
+  return true;
+}
+
+bool goto_trace_step_assertt::is_assert() const
+{
+  return true;
+}
+
+bool goto_trace_step_gotot::is_goto() const
+{
+  return true;
+}
+
+bool goto_trace_step_constraintt::is_constraint() const
+{
+  return true;
+}
+
+bool goto_trace_step_function_callt::is_function_call() const
+{
+  return true;
+}
+
+bool goto_trace_step_function_returnt::is_function_return() const
+{
+  return true;
+}
+
+bool goto_trace_step_locationt::is_location() const
+{
+  return true;
+}
+
+bool goto_trace_step_outputt::is_output() const
+{
+  return true;
+}
+
+bool goto_trace_step_inputt::is_input() const
+{
+  return true;
+}
+
+bool goto_trace_step_declt::is_decl() const
+{
+  return true;
+}
+
+bool goto_trace_step_deadt::is_dead() const
+{
+  return true;
+}
+
+bool goto_trace_step_shared_readt::is_shared_read() const
+{
+  return true;
+}
+
+bool goto_trace_step_shared_writet::is_shared_write() const
+{
+  return true;
+}
+
+bool goto_trace_step_spawnt::is_spawn() const
+{
+  return true;
+}
+
+bool goto_trace_step_memory_barriert::is_memory_barrier() const
+{
+  return true;
+}
+
+bool goto_trace_step_atomic_begint::is_atomic_begin() const
+{
+  return true;
+}
+
+bool goto_trace_step_atomic_endt::is_atomic_end() const
+{
+  return true;
+}
+
+safe_goto_tracet::iteratort safe_goto_tracet::begin()
+{
+  return make_dereference_iterator(steps_.begin());
+}
+
+safe_goto_tracet::const_iteratort safe_goto_tracet::begin() const
+{
+  return make_dereference_iterator(steps_.begin());
+}
+
+safe_goto_tracet::const_iteratort safe_goto_tracet::cbegin() const
+{
+  return make_dereference_iterator(steps_.begin());
+}
+
+safe_goto_tracet::iteratort safe_goto_tracet::end()
+{
+  return make_dereference_iterator(steps_.end());
+}
+
+safe_goto_tracet::const_iteratort safe_goto_tracet::end() const
+{
+  return make_dereference_iterator(steps_.end());
+}
+
+safe_goto_tracet::const_iteratort safe_goto_tracet::cend() const
+{
+  return make_dereference_iterator(steps_.end());
+}
--- src/goto-programs/safe_goto_trace.h
+++ src/goto-programs/safe_goto_trace.h
@@ -12,6 +12,7 @@ Author: @reuk
 #ifndef CPROVER_GOTO_PROGRAMS_SAFE_GOTO_TRACE_H
 #define CPROVER_GOTO_PROGRAMS_SAFE_GOTO_TRACE_H
 
+#include <util/dereference_iterator.h>
 #include <util/make_unique.h>
 
 #include <iosfwd>
@@ -42,24 +43,24 @@ class goto_trace_visitort
 public:
   virtual ~goto_trace_visitort() = default;
 
-  virtual void visit(goto_trace_step_assignmentt) = 0;
-  virtual void visit(goto_trace_step_assumet) = 0;
-  virtual void visit(goto_trace_step_assertt) = 0;
-  virtual void visit(goto_trace_step_gotot) = 0;
-  virtual void visit(goto_trace_step_constraintt) = 0;
-  virtual void visit(goto_trace_step_function_callt) = 0;
-  virtual void visit(goto_trace_step_function_returnt) = 0;
-  virtual void visit(goto_trace_step_locationt) = 0;
-  virtual void visit(goto_trace_step_outputt) = 0;
-  virtual void visit(goto_trace_step_inputt) = 0;
-  virtual void visit(goto_trace_step_declt) = 0;
-  virtual void visit(goto_trace_step_deadt) = 0;
-  virtual void visit(goto_trace_step_shared_readt) = 0;
-  virtual void visit(goto_trace_step_shared_writet) = 0;
-  virtual void visit(goto_trace_step_spawnt) = 0;
-  virtual void visit(goto_trace_step_memory_barriert) = 0;
-  virtual void visit(goto_trace_step_atomic_begint) = 0;
-  virtual void visit(goto_trace_step_atomic_endt) = 0;
+  virtual void visit(goto_trace_step_assignmentt &) = 0;
+  virtual void visit(goto_trace_step_assumet &) = 0;
+  virtual void visit(goto_trace_step_assertt &) = 0;
+  virtual void visit(goto_trace_step_gotot &) = 0;
+  virtual void visit(goto_trace_step_constraintt &) = 0;
+  virtual void visit(goto_trace_step_function_callt &) = 0;
+  virtual void visit(goto_trace_step_function_returnt &) = 0;
+  virtual void visit(goto_trace_step_locationt &) = 0;
+  virtual void visit(goto_trace_step_outputt &) = 0;
+  virtual void visit(goto_trace_step_inputt &) = 0;
+  virtual void visit(goto_trace_step_declt &) = 0;
+  virtual void visit(goto_trace_step_deadt &) = 0;
+  virtual void visit(goto_trace_step_shared_readt &) = 0;
+  virtual void visit(goto_trace_step_shared_writet &) = 0;
+  virtual void visit(goto_trace_step_spawnt &) = 0;
+  virtual void visit(goto_trace_step_memory_barriert &) = 0;
+  virtual void visit(goto_trace_step_atomic_begint &) = 0;
+  virtual void visit(goto_trace_step_atomic_endt &) = 0;
 };
 
 class goto_trace_const_visitort
@@ -67,24 +68,24 @@ class goto_trace_const_visitort
 public:
   virtual ~goto_trace_const_visitort() = default;
 
-  virtual void visit(goto_trace_step_assignmentt) const = 0;
-  virtual void visit(goto_trace_step_assumet) const = 0;
-  virtual void visit(goto_trace_step_assertt) const = 0;
-  virtual void visit(goto_trace_step_gotot) const = 0;
-  virtual void visit(goto_trace_step_constraintt) const = 0;
-  virtual void visit(goto_trace_step_function_callt) const = 0;
-  virtual void visit(goto_trace_step_function_returnt) const = 0;
-  virtual void visit(goto_trace_step_locationt) const = 0;
-  virtual void visit(goto_trace_step_outputt) const = 0;
-  virtual void visit(goto_trace_step_inputt) const = 0;
-  virtual void visit(goto_trace_step_declt) const = 0;
-  virtual void visit(goto_trace_step_deadt) const = 0;
-  virtual void visit(goto_trace_step_shared_readt) const = 0;
-  virtual void visit(goto_trace_step_shared_writet) const = 0;
-  virtual void visit(goto_trace_step_spawnt) const = 0;
-  virtual void visit(goto_trace_step_memory_barriert) const = 0;
-  virtual void visit(goto_trace_step_atomic_begint) const = 0;
-  virtual void visit(goto_trace_step_atomic_endt) const = 0;
+  virtual void visit(goto_trace_step_assignmentt &) const = 0;
+  virtual void visit(goto_trace_step_assumet &) const = 0;
+  virtual void visit(goto_trace_step_assertt &) const = 0;
+  virtual void visit(goto_trace_step_gotot &) const = 0;
+  virtual void visit(goto_trace_step_constraintt &) const = 0;
+  virtual void visit(goto_trace_step_function_callt &) const = 0;
+  virtual void visit(goto_trace_step_function_returnt &) const = 0;
+  virtual void visit(goto_trace_step_locationt &) const = 0;
+  virtual void visit(goto_trace_step_outputt &) const = 0;
+  virtual void visit(goto_trace_step_inputt &) const = 0;
+  virtual void visit(goto_trace_step_declt &) const = 0;
+  virtual void visit(goto_trace_step_deadt &) const = 0;
+  virtual void visit(goto_trace_step_shared_readt &) const = 0;
+  virtual void visit(goto_trace_step_shared_writet &) const = 0;
+  virtual void visit(goto_trace_step_spawnt &) const = 0;
+  virtual void visit(goto_trace_step_memory_barriert &) const = 0;
+  virtual void visit(goto_trace_step_atomic_begint &) const = 0;
+  virtual void visit(goto_trace_step_atomic_endt &) const = 0;
 };
 
 class goto_trace_visitor_const_argst
@@ -92,24 +93,24 @@ class goto_trace_visitor_const_argst
 public:
   virtual ~goto_trace_visitor_const_argst() = default;
 
-  virtual void visit(const goto_trace_step_assignmentt) = 0;
-  virtual void visit(const goto_trace_step_assumet) = 0;
-  virtual void visit(const goto_trace_step_assertt) = 0;
-  virtual void visit(const goto_trace_step_gotot) = 0;
-  virtual void visit(const goto_trace_step_constraintt) = 0;
-  virtual void visit(const goto_trace_step_function_callt) = 0;
-  virtual void visit(const goto_trace_step_function_returnt) = 0;
-  virtual void visit(const goto_trace_step_locationt) = 0;
-  virtual void visit(const goto_trace_step_outputt) = 0;
-  virtual void visit(const goto_trace_step_inputt) = 0;
-  virtual void visit(const goto_trace_step_declt) = 0;
-  virtual void visit(const goto_trace_step_deadt) = 0;
-  virtual void visit(const goto_trace_step_shared_readt) = 0;
-  virtual void visit(const goto_trace_step_shared_writet) = 0;
-  virtual void visit(const goto_trace_step_spawnt) = 0;
-  virtual void visit(const goto_trace_step_memory_barriert) = 0;
-  virtual void visit(const goto_trace_step_atomic_begint) = 0;
-  virtual void visit(const goto_trace_step_atomic_endt) = 0;
+  virtual void visit(const goto_trace_step_assignmentt &) = 0;
+  virtual void visit(const goto_trace_step_assumet &) = 0;
+  virtual void visit(const goto_trace_step_assertt &) = 0;
+  virtual void visit(const goto_trace_step_gotot &) = 0;
+  virtual void visit(const goto_trace_step_constraintt &) = 0;
+  virtual void visit(const goto_trace_step_function_callt &) = 0;
+  virtual void visit(const goto_trace_step_function_returnt &) = 0;
+  virtual void visit(const goto_trace_step_locationt &) = 0;
+  virtual void visit(const goto_trace_step_outputt &) = 0;
+  virtual void visit(const goto_trace_step_inputt &) = 0;
+  virtual void visit(const goto_trace_step_declt &) = 0;
+  virtual void visit(const goto_trace_step_deadt &) = 0;
+  virtual void visit(const goto_trace_step_shared_readt &) = 0;
+  virtual void visit(const goto_trace_step_shared_writet &) = 0;
+  virtual void visit(const goto_trace_step_spawnt &) = 0;
+  virtual void visit(const goto_trace_step_memory_barriert &) = 0;
+  virtual void visit(const goto_trace_step_atomic_begint &) = 0;
+  virtual void visit(const goto_trace_step_atomic_endt &) = 0;
 };
 
 class goto_trace_const_visitor_const_argst
@@ -117,24 +118,24 @@ class goto_trace_const_visitor_const_argst
 public:
   virtual ~goto_trace_const_visitor_const_argst() = default;
 
-  virtual void visit(const goto_trace_step_assignmentt) const = 0;
-  virtual void visit(const goto_trace_step_assumet) const = 0;
-  virtual void visit(const goto_trace_step_assertt) const = 0;
-  virtual void visit(const goto_trace_step_gotot) const = 0;
-  virtual void visit(const goto_trace_step_constraintt) const = 0;
-  virtual void visit(const goto_trace_step_function_callt) const = 0;
-  virtual void visit(const goto_trace_step_function_returnt) const = 0;
-  virtual void visit(const goto_trace_step_locationt) const = 0;
-  virtual void visit(const goto_trace_step_outputt) const = 0;
-  virtual void visit(const goto_trace_step_inputt) const = 0;
-  virtual void visit(const goto_trace_step_declt) const = 0;
-  virtual void visit(const goto_trace_step_deadt) const = 0;
-  virtual void visit(const goto_trace_step_shared_readt) const = 0;
-  virtual void visit(const goto_trace_step_shared_writet) const = 0;
-  virtual void visit(const goto_trace_step_spawnt) const = 0;
-  virtual void visit(const goto_trace_step_memory_barriert) const = 0;
-  virtual void visit(const goto_trace_step_atomic_begint) const = 0;
-  virtual void visit(const goto_trace_step_atomic_endt) const = 0;
+  virtual void visit(const goto_trace_step_assignmentt &) const = 0;
+  virtual void visit(const goto_trace_step_assumet &) const = 0;
+  virtual void visit(const goto_trace_step_assertt &) const = 0;
+  virtual void visit(const goto_trace_step_gotot &) const = 0;
+  virtual void visit(const goto_trace_step_constraintt &) const = 0;
+  virtual void visit(const goto_trace_step_function_callt &) const = 0;
+  virtual void visit(const goto_trace_step_function_returnt &) const = 0;
+  virtual void visit(const goto_trace_step_locationt &) const = 0;
+  virtual void visit(const goto_trace_step_outputt &) const = 0;
+  virtual void visit(const goto_trace_step_inputt &) const = 0;
+  virtual void visit(const goto_trace_step_declt &) const = 0;
+  virtual void visit(const goto_trace_step_deadt &) const = 0;
+  virtual void visit(const goto_trace_step_shared_readt &) const = 0;
+  virtual void visit(const goto_trace_step_shared_writet &) const = 0;
+  virtual void visit(const goto_trace_step_spawnt &) const = 0;
+  virtual void visit(const goto_trace_step_memory_barriert &) const = 0;
+  virtual void visit(const goto_trace_step_atomic_begint &) const = 0;
+  virtual void visit(const goto_trace_step_atomic_endt &) const = 0;
 };
 
 class namespacet;
@@ -151,78 +152,24 @@ public:
 
   virtual std::unique_ptr<goto_trace_step_baset> clone() const = 0;
 
-  virtual bool is_assignment() const
-  {
-    return false;
-  }
-  virtual bool is_assume() const
-  {
-    return false;
-  }
-  virtual bool is_assert() const
-  {
-    return false;
-  }
-  virtual bool is_goto() const
-  {
-    return false;
-  }
-  virtual bool is_constraint() const
-  {
-    return false;
-  }
-  virtual bool is_function_call() const
-  {
-    return false;
-  }
-  virtual bool is_function_return() const
-  {
-    return false;
-  }
-  virtual bool is_location() const
-  {
-    return false;
-  }
-  virtual bool is_output() const
-  {
-    return false;
-  }
-  virtual bool is_input() const
-  {
-    return false;
-  }
-  virtual bool is_decl() const
-  {
-    return false;
-  }
-  virtual bool is_dead() const
-  {
-    return false;
-  }
-  virtual bool is_shared_read() const
-  {
-    return false;
-  }
-  virtual bool is_shared_write() const
-  {
-    return false;
-  }
-  virtual bool is_spawn() const
-  {
-    return false;
-  }
-  virtual bool is_memory_barrier() const
-  {
-    return false;
-  }
-  virtual bool is_atomic_begin() const
-  {
-    return false;
-  }
-  virtual bool is_atomic_end() const
-  {
-    return false;
-  }
+  virtual bool is_assignment() const;
+  virtual bool is_assume() const;
+  virtual bool is_assert() const;
+  virtual bool is_goto() const;
+  virtual bool is_constraint() const;
+  virtual bool is_function_call() const;
+  virtual bool is_function_return() const;
+  virtual bool is_location() const;
+  virtual bool is_output() const;
+  virtual bool is_input() const;
+  virtual bool is_decl() const;
+  virtual bool is_dead() const;
+  virtual bool is_shared_read() const;
+  virtual bool is_shared_write() const;
+  virtual bool is_spawn() const;
+  virtual bool is_memory_barrier() const;
+  virtual bool is_atomic_begin() const;
+  virtual bool is_atomic_end() const;
 
   void output(const namespacet &, std::ostream &);
 
@@ -288,187 +235,140 @@ class goto_trace_step_assignmentt final
   : public detail::goto_trace_step_mixint<goto_trace_step_assignmentt>
 {
 public:
-  bool is_assignment() const override
-  {
-    return true;
-  }
+  bool is_assignment() const override;
 };
 
 class goto_trace_step_assumet final
   : public detail::goto_trace_step_mixint<goto_trace_step_assumet>
 {
 public:
-  bool is_assume() const override
-  {
-    return true;
-  }
+  bool is_assume() const override;
 };
 
 class goto_trace_step_assertt final
   : public detail::goto_trace_step_mixint<goto_trace_step_assertt>
 {
 public:
-  bool is_assert() const override
-  {
-    return true;
-  }
+  bool is_assert() const override;
 };
 
 class goto_trace_step_gotot final
   : public detail::goto_trace_step_mixint<goto_trace_step_gotot>
 {
 public:
-  bool is_goto() const override
-  {
-    return true;
-  }
+  bool is_goto() const override;
 };
 
-class goto_trace_step_constaintt final
-  : public detail::goto_trace_step_mixint<goto_trace_step_constaintt>
+class goto_trace_step_constraintt final
+  : public detail::goto_trace_step_mixint<goto_trace_step_constraintt>
 {
 public:
-  bool is_constraint() const override
-  {
-    return true;
-  }
+  bool is_constraint() const override;
 };
 
 class goto_trace_step_function_callt final
   : public detail::goto_trace_step_mixint<goto_trace_step_function_callt>
 {
 public:
-  bool is_function_call() const override
-  {
-    return true;
-  }
+  bool is_function_call() const override;
 };
 
 class goto_trace_step_function_returnt final
   : public detail::goto_trace_step_mixint<goto_trace_step_function_returnt>
 {
 public:
-  bool is_function_return() const override
-  {
-    return true;
-  }
+  bool is_function_return() const override;
 };
 
 class goto_trace_step_locationt final
   : public detail::goto_trace_step_mixint<goto_trace_step_locationt>
 {
 public:
-  bool is_location() const override
-  {
-    return true;
-  }
+  bool is_location() const override;
 };
 
 class goto_trace_step_outputt final
   : public detail::goto_trace_step_mixint<goto_trace_step_outputt>
 {
 public:
-  bool is_output() const override
-  {
-    return true;
-  }
+  bool is_output() const override;
 };
 
 class goto_trace_step_inputt final
   : public detail::goto_trace_step_mixint<goto_trace_step_inputt>
 {
 public:
-  bool is_input() const override
-  {
-    return true;
-  }
+  bool is_input() const override;
 };
 
 class goto_trace_step_declt final
   : public detail::goto_trace_step_mixint<goto_trace_step_declt>
 {
 public:
-  bool is_decl() const override
-  {
-    return true;
-  }
+  bool is_decl() const override;
 };
 
 class goto_trace_step_deadt final
   : public detail::goto_trace_step_mixint<goto_trace_step_deadt>
 {
 public:
-  bool is_dead() const override
-  {
-    return true;
-  }
+  bool is_dead() const override;
 };
 
 class goto_trace_step_shared_readt final
   : public detail::goto_trace_step_mixint<goto_trace_step_shared_readt>
 {
 public:
-  bool is_shared_read() const override
-  {
-    return true;
-  }
+  bool is_shared_read() const override;
 };
 
 class goto_trace_step_shared_writet final
   : public detail::goto_trace_step_mixint<goto_trace_step_shared_writet>
 {
 public:
-  bool is_shared_write() const override
-  {
-    return true;
-  }
+  bool is_shared_write() const override;
 };
 
 class goto_trace_step_spawnt final
   : public detail::goto_trace_step_mixint<goto_trace_step_spawnt>
 {
 public:
-  bool is_spawn() const override
-  {
-    return true;
-  }
+  bool is_spawn() const override;
 };
 
 class goto_trace_step_memory_barriert final
   : public detail::goto_trace_step_mixint<goto_trace_step_memory_barriert>
 {
 public:
-  bool is_memory_barrier() const override
-  {
-    return true;
-  }
+  bool is_memory_barrier() const override;
 };
 
 class goto_trace_step_atomic_begint final
   : public detail::goto_trace_step_mixint<goto_trace_step_atomic_begint>
 {
 public:
-  bool is_atomic_begin() const override
-  {
-    return true;
-  }
+  bool is_atomic_begin() const override;
 };
 
 class goto_trace_step_atomic_endt final
   : public detail::goto_trace_step_mixint<goto_trace_step_atomic_endt>
 {
 public:
-  bool is_atomic_end() const override
-  {
-    return true;
-  }
+  bool is_atomic_end() const override;
 };
 
 class goto_tracet;
 
 class safe_goto_tracet final
 {
+  using stepst = std::list<std::unique_ptr<goto_trace_step_baset>>;
+
 public:
+  using iteratort = dereference_iteratort<stepst::iterator>;
+  using const_iteratort = dereference_iteratort<stepst::const_iterator>;
+
+  safe_goto_tracet() = default;
+
   // [[deprecated]]
   explicit safe_goto_tracet(const goto_tracet &trace);
 
@@ -476,6 +376,14 @@ public:
   goto_tracet make_unsafe() const;
 
   void output(const namespacet &ns, std::ostream &os) const;
+
+  iteratort begin();
+  const_iteratort begin() const;
+  const_iteratort cbegin() const;
+
+  iteratort end();
+  const_iteratort end() const;
+  const_iteratort cend() const;
 
 private:
   std::list<std::unique_ptr<goto_trace_step_baset>> steps_;
